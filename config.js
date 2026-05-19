0c1a9e0f7cc32b5cf0fafdc3b1ceac51ae4e8a74
PerolaSupermercado <midiakitperola@gmail.com>
2026-05-19 17:43:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,81 +1,15 @@
 const CONFIGURACOES = {
-
   titulo: "Escolha uma categoria e confira nossas promoções",
 
   subtitulo: "",
 
-  banner: "img/banner.jpg",
+  bannerPadrao: "img/banner.jpg",
+
+  placeholder: "img/placeholder.png",
 
   linkGrupo:
     "https://chat.whatsapp.com/HCEt8q7P5vgDIPVq8B68Du?mode=gi_t"
-
 };
 
-const OFERTAS = {
-
-  "Super Ofertas": {
-
-    inicio: "2026-05-18T16:00:00-03:00",
-
-    fim: "2026-05-18T23:59:00-03:00",
-
-    imagens: [
-
-      "img/superofertas01.jpg"
-
-    ]
-
-  },
-
-  "Hortifruti Fresquinho": {
-
-    inicio: "2026-05-19T05:00:00-03:00",
-
-    fim: "2026-05-20T23:59:00-03:00",
-
-    imagens: [
-
-      "img/OFERTA-HORTIFRUTI01.jpg"
-
-    ]
-
-  },
-
-  "Quinta do Filé": {
-
-    inicio: "2026-10-21T21:00:00-03:00",
-
-    fim: "2026-10-22T23:59:00-03:00",
-
-    imagens: []
-
-  },
-
-  "Ofertas de Fim de Semana": {
-
-    inicio: "2026-10-22T21:00:00-03:00",
-
-    fim: "2026-10-25T23:59:00-03:00",
-
-    imagens: []
-
-  },
-
-  "Vem Aproveitar!": {
-
-    inicio: "2026-05-19T05:00:00-03:00",
-
-    fim: "2026-05-20T23:59:00-03:00",
-
-    imagens: [
-
-      "img/SUPER OFERTAS.jpg",
-
-      "img/SUPER OFERTAS2.jpg"
-
-    ]
-
-  } 
-};
 const PLANILHA_OFERTAS =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vRNYQCZTP97BOoMz2wmFsr1MAIzfu2XgAGr0Th76LbMm2c7nmW3h-vj8rJe9rtvop1QzPg8Yt9lSCzp/pub?output=csv";
